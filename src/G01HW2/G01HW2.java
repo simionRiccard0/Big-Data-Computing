e19083e9997852c7bc0fd5a29b93654f8f8fce69
F-E-R <francescoelia.rizzo@studenti.unipd.it>
2026-06-16 11:44:33 +0200

    Brackets.

--- src/G01HW2/G01HW2.java
+++ src/G01HW2/G01HW2.java
@@ -8,44 +8,50 @@ import scala.Tuple2;
 import java.util.*;
 import java.util.concurrent.Semaphore;
 
-public class G01HW2 {
-
+public class G01HW2
+{
     // Hash-function parameters for Count-Min Sketch
     static final long P = 8191L; // prime p used in the 2-universal family
 
     // Generate a hash function h(x) = ((a*x + b) mod p) mod C; returns { a, b }
-    static long[] generateHashParams(Random rnd) {
+    static long[] generateHashParams(Random rnd)
+    {
         long a = 1 + (long)(rnd.nextDouble() * (P - 1));  // a in [1, P-1]
         long b = (long)(rnd.nextDouble() * P);            // b in [0, P-1]
         return new long[]{a, b};
     }
 
     // Evaluate h_j(x) = ((a*x + b) mod p) mod C
-    static int hashValue(long x, long a, long b, int C) {
+    static int hashValue(long x, long a, long b, int C)
+    {
         long val = ((a * x + b) % P + P) % P;
         return (int)(val % C);
     }
 
     // Update Count-Min Sketch with one item
-    static void cmUpdate(long item, long[][] sketch, long[][] hashParams, int d, int w) {
-        for (int j = 0; j < d; j++) {
+    static void cmUpdate(long item, long[][] sketch, long[][] hashParams, int d, int w)
+    {
+        for (int j = 0; j < d; j++)
+        {
             int k = hashValue(item, hashParams[j][0], hashParams[j][1], w);
             sketch[j][k]++;
         }
     }
 
     // Estimate frequency of item using Count-Min Sketch
-    static long cmEstimate(long item, long[][] sketch, long[][] hashParams, int d, int w) {
+    static long cmEstimate(long item, long[][] sketch, long[][] hashParams, int d, int w)
+    {
         long minVal = Long.MAX_VALUE;
-        for (int j = 0; j < d; j++) {
+        for (int j = 0; j < d; j++)
+        {
             int k = hashValue(item, hashParams[j][0], hashParams[j][1], w);
             minVal = Math.min(minVal, sketch[j][k]);
         }
         return minVal;
     }
 
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws Exception
+    {
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // INPUT READING
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
@@ -85,7 +91,8 @@ public class G01HW2 {
         long[][] sketch = new long[d][w];
         Random rnd = new Random();
         long[][] hashParams = new long[d][2];
-        for (int j = 0; j < d; j++) {
+        for (int j = 0; j < d; j++)
+        {
             long[] params = generateHashParams(rnd);
             hashParams[j][0] = params[0];
             hashParams[j][1] = params[1];
@@ -115,10 +122,12 @@ public class G01HW2 {
         sc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevels.MEMORY_AND_DISK)
                 .foreachRDD((batch, time) -> {
                      // this is working on the batch at time *time*
-                    if (streamLength[0] < n) {
+                    if (streamLength[0] < n)
+                    {
                         long batchSize = batch.count();
                         streamLength[0] += batchSize;
-                        if (batchSize > 0) {
+                        if (batchSize > 0)
+                        {
                             // Collect distinct items with their frequencies in this batch
                             // (use i1+i2 to count, NOT the deduplication bug i1->1L)
                             Map<Long, Long> batchItems = batch
@@ -129,7 +138,8 @@ public class G01HW2 {
                             // Update the streaming state. If the overall count of processed items reaches the
                             // THRESHOLD value (among all batches processed so far), subsequent items of the
                             // current batch are ignored, and no further batches will be processed
-                            for (Map.Entry<Long, Long> entry : batchItems.entrySet()) {
+                            for (Map.Entry<Long, Long> entry : batchItems.entrySet())
+                            {
                                 long item  = entry.getKey();
                                 long count = entry.getValue();
 
@@ -137,20 +147,20 @@ public class G01HW2 {
                                 histogram.merge(item, count, Long::sum);
 
                                 // Update Sticky Sampling
-                                if (stickyDict.containsKey(item)) {
+                                if (stickyDict.containsKey(item))
                                     stickyDict.merge(item, count, Long::sum);
-                                } else {
+                                else
+                                {
                                     // Each individual occurrence gets a Bernoulli trial with prob r/n
                                     // (approximation: treat the batch count as individual arrivals)
                                     long added = 0L;
-                                    for (long i = 0; i < count; i++) {
-                                        if (rnd.nextDouble() <= (double) r / (double) n) {
+                                    for (long i = 0; i < count; i++)
+                                    {
+                                        if (rnd.nextDouble() <= (double) r / (double) n)
                                             added++;
-                                        }
                                     }
-                                    if (added > 0) {
+                                    if (added > 0)
                                         stickyDict.put(item, added);
-                                    }
                                 }
 
                                 // Update Count-Min Sketch (once per actual occurrence)
@@ -158,15 +168,16 @@ public class G01HW2 {
                                     cmUpdate(item, sketch, hashParams, d, w);
 
                                 // Check if item should be added to FCM
-                                if (!fcmSet.contains(item)) {
+                                if (!fcmSet.contains(item))
+                                {
                                     long est = cmEstimate(item, sketch, hashParams, d, w);
-                                    if (est >= phi * n) {
+                                    if (est >= phi * n)
                                         fcmSet.add(item);
-                                    }
                                 }
                             }
 
-                            if (streamLength[0] >= n) {
+                            if (streamLength[0] >= n)
+                            {
                                 stoppingSemaphore.release();
                             }
                         }
@@ -206,36 +217,36 @@ public class G01HW2 {
         // COMPUTE AND PRINT RESULTS
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        long threshold = (long)(phi * n); // items with freq >= phi*n are true frequent
+        long threshold = (long)(phi * n); // Items with freq >= phi*n are true frequent
 
         // --- TRUE FREQUENT ITEMS ---
         List<Long> trueFrequent = new ArrayList<>();
-        for (Map.Entry<Long, Long> e : histogram.entrySet()) {
-            if (e.getValue() >= threshold) {
+        for (Map.Entry<Long, Long> e : histogram.entrySet())
+        {
+            if (e.getValue() >= threshold)
                 trueFrequent.add(e.getKey());
-            }
         }
         Collections.sort(trueFrequent);
 
         System.out.println("TRUE FREQUENT ITEMS");
-        for (long item : trueFrequent) {
+        for (long item : trueFrequent)
             System.out.println("Item = " + item + " True Freq = " + histogram.get(item));
-        }
 
         // --- STICKY SAMPLING ---
         // FSS: items in stickyDict with count >= (phi - epsilon) * n
         double sssThreshold = (phi - epsilon) * n;
         List<Long> fss = new ArrayList<>();
-        for (Map.Entry<Long, Long> e : stickyDict.entrySet()) {
-            if (e.getValue() >= sssThreshold) {
+        for (Map.Entry<Long, Long> e : stickyDict.entrySet())
+        {
+            if (e.getValue() >= sssThreshold)
                 fss.add(e.getKey());
-            }
         }
         Collections.sort(fss);
 
         System.out.println("STICKY SAMPLING");
         System.out.println("Size of dictionary = " + stickyDict.size());
-        for (long item : fss) {
+        for (long item : fss)
+        {
             long trueFreq = histogram.getOrDefault(item, 0L);
             System.out.println("Item = " + item + " True Freq = " + trueFreq);
         }
@@ -246,7 +257,8 @@ public class G01HW2 {
 
         System.out.println("COUNT-MIN SKETCH");
         System.out.println("Size of F_CM = " + fcmList.size());
-        for (long item : fcmList) {
+        for (long item : fcmList)
+        {
             long trueFreq = histogram.getOrDefault(item, 0L);
             System.out.println("Item = " + item + " True Freq = " + trueFreq);
         }
